467650f4e9e09867b24004223ebefc7dc2afef7b
nick.fox <nick.fox@sky.uk>
2016-06-30 18:49:22 +0100

    initial add

---
+++ stock.pl
@@ -0,0 +1,62 @@
+#!/usr/bin/perl
+
+#use strict;
+use warnings;
+
+
+print 'hello';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+__END__
+
+
+profit minimum £50 (5000)
+
+cost per buy/sell transaction
+£25
+0.5% Or £1 on each transaction which ever is greater
+
+0.5% stamp duty on purchases only
+
+
+what is the relationship between value of shares and share increase to make a sale profit of £50?
+
+buy
+sky 850p
+
+200 x 850 = 170,000 (1700)
+or
+170,000 / 850 = 200
+investment / share price = number of shares
++ 0.5% stamp = 8.5
++ 0.5% transaction fee = 8.5
++ 12.5 broker fee
+cost == 1729.50
+
+sell @ 900
+200 x 900 = 180,000 (1800)
+- 0.5% £9
+- 12.5 broker fee
+return == 1778.5
+
+ROI
+1778.5
+- 1729.50
+== £49
+
+
+
+
